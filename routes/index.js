e771e7a6093a9b102a6384d596d3e623872654c8
Juan P Gomez <suanchi@gmail.com>
2015-06-20 16:52:48 -0300

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ router.get('/author', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load); // autoload :quizId
+router.param('quizId', 		quizController.load); 		// autoload :quizId
+router.param('commentId',	commentController.load);	// autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', 	sessionController.new);		// formulario de login
@@ -36,7 +37,8 @@ router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizContr
 router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
 // Definicion de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',						commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',							commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',	sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
